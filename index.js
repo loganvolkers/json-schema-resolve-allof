bc6a59b92dcdf35b95088ebccfc38220dd5c7535
CSoellinger <cs@pixelcrab.at>
2018-01-18 09:09:39 +0100

    refactor(TypeScript): Make code more TypeScript like and update JavaScript files

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var _ = require('lodash');
+"use strict";
+exports.__esModule = true;
+var _ = require("lodash");
 module.exports = function resolveAllOf(inputSpec) {
-    var out;
     if (inputSpec && typeof inputSpec === 'object') {
-        if (inputSpec.allOf) {
-            var allOf = inputSpec.allOf;
-            delete inputSpec.allOf;
-            var nested = _.mergeWith.apply(_, [{}].concat(allOf, [customizer]));
-            out = _.defaults(inputSpec, nested, customizer);
+        if (Object.keys(inputSpec).length > 0) {
+            if (inputSpec.allOf) {
+                var allOf = inputSpec.allOf;
+                delete inputSpec.allOf;
+                var nested = _.mergeWith.apply(_, [{}].concat(allOf, [customizer]));
+                inputSpec = _.defaults(inputSpec, nested, customizer);
+            }
+            Object.keys(inputSpec).forEach(function (key) {
+                inputSpec[key] = resolveAllOf(inputSpec[key]);
+            });
         }
-        else {
-            out = inputSpec;
-        }
-        Object.keys(out).forEach(function (key, context) {
-            out[key] = resolveAllOf(out[key]);
-        });
-    }
-    else {
-        out = inputSpec;
     }
     return inputSpec;
 };
