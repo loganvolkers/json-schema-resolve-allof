c192ea586a4d7bb5b407dbd949d1a551ba70071e
Sergii B <forrest-ua@users.noreply.github.com>
2018-10-25 19:34:52 +0200

    feature: Recursive all of (#5)
    * (feature) Add resursive resolve for allOf.
    * fix: linting error and update tests

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 import * as _ from 'lodash';
 
 function resolveAllOf(inputSpec: any): any {
-    if (inputSpec && typeof inputSpec === 'object') {
-        if (Object.keys(inputSpec).length > 0) {
-            if (inputSpec.allOf) {
-                const allOf = inputSpec.allOf;
-                delete inputSpec.allOf;
-                const nested = _.mergeWith({}, ...allOf, customizer);
-                inputSpec = _.defaults(inputSpec, nested, customizer);
-            }
-            Object.keys(inputSpec).forEach((key: string) => {
-                inputSpec[key] = resolveAllOf(inputSpec[key]);
-            });
+    if (inputSpec && typeof inputSpec === 'object'
+        && Object.keys(inputSpec).length > 0
+    ) {
+        if (inputSpec.allOf) {
+            const allOf = inputSpec.allOf;
+            delete inputSpec.allOf;
+            const nested = _.mergeWith({}, ...allOf, customizer);
+            inputSpec = _.defaultsDeep(inputSpec, nested, customizer);
         }
+        Object.keys(inputSpec).forEach((key: string) => {
+            inputSpec[key] = resolveAllOf(inputSpec[key]);
+        });
     }
     return inputSpec;
 }
